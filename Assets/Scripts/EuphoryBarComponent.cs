1604cce4b9a20e9e467d656d2276b8c631e53afe
Maruxir <91840279+Maruxir@users.noreply.github.com>
2021-11-29 13:01:00 +0100

    Add files via upload

---
+++ Assets/Scripts/EuphoryBarComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EuphoryBarComponent : MonoBehaviour
+
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetEuphory(int euphory)
+    {
+        slider.value = euphory;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void SetMinEuphory(int euphory)
+    {
+        slider.minValue = euphory;
+        slider.value = euphory;
+        fill.color = gradient.Evaluate(1f);
+    }
+}
